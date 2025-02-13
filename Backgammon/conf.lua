376a5c11102c08241402a48ad13e453fa3da116b
Alex Trister <alexthetrister@gmail.com>
2025-02-13 15:49:20 -0500

    Piece Positioning System complete

--- Backgammon/conf.lua
+++ Backgammon/conf.lua
@@ -1,4 +1,5 @@
 function love.conf(t)
     t.window.fullscreen = true      -- Enable fullscreen mode
     t.window.fullscreentype = "desktop"  -- Fullscreen type can be "desktop" or "exclusive"
+    t.console = true
 end
--- Backgammon/main.lua
+++ Backgammon/main.lua
@@ -1,10 +1,63 @@
 -- Libraries
 
--- Declare variable
+
 
 function love.load()
-  blackChip = love.graphics.newImage("Images/Black_Backgammon.png")
+  draw = true
+  numOfPieces = nil
+  pieceType = nil
+  pieceLocation = nil
+
+  -- Creating piece storage
+  pieces = {{{2,1, 391}, {0,"", 546}, {0,"", 701}, {0,"", 856}, {0,"", 1011}, {5,2, 1166}, {0,"", 1633}, {3,2, 1788}, {0,"", 1943}, {0,"", 2098}, {0,"", 2253}, {5,1, 2408}},
+            {{2,2, 391}, {0,"", 546}, {0,"", 701}, {0,"", 856}, {0,"", 1011}, {5,1, 1166}, {0,"", 1633}, {3,1, 1788}, {0,"", 1943}, {0,"", 2098}, {0,"", 2253}, {5,2, 2408}}}
+
+  -- Creating the image objects
   background = love.graphics.newImage("Images/Backgammon_Board_blender_concept-with-pieces.png")
+
+  whitePiece = love.graphics.newImage("Images/White_Piece.png")
+  blackPiece = love.graphics.newImage("Images/Black_Piece.png")
+end
+
+function drawPieces()
+  print("Drawing pieces")
+  for i = 1, #pieces do
+    for j = 1, #pieces[i] do
+          -- Setting how many pieces are in the location
+          numOfPieces = pieces[i][j][1]
+          print("Number of pieces: "..numOfPieces)
+          -- Setting the type of piece
+          pieceType = pieces[i][j][2]
+          print("Type of piece: "..pieceType)
+          -- Setting the location of the piece
+          pieceLocation = pieces[i][j][3]
+          print("Location of piece: "..pieceLocation)
+
+        if numOfPieces > 0 then
+          if i == 1 then
+            local y = 0
+            for l = 1, numOfPieces do
+              if pieceType == 2 then
+                love.graphics.draw(blackPiece, pieceLocation - (148 / 2), y)
+              else
+                love.graphics.draw(whitePiece, pieceLocation - (148 / 2), y)
+              end
+              y = y + 100
+            end
+          elseif i == 2 then
+            local y = 1439 - 148
+            for h = 1, numOfPieces do
+              if pieceType == 2 then
+                love.graphics.draw(blackPiece, pieceLocation - (148 / 2), y)
+              else
+                love.graphics.draw(whitePiece, pieceLocation - (148 / 2), y)
+              end
+              y = y - 100
+            end
+          end
+      end
+    end
+  end
 end
 
 function love.update(dt)
@@ -22,13 +75,18 @@ function love.update(dt)
     mouseCoordsY = 0  
   end
   
-  
 end
 
 function love.draw()
+  -- if draw then
   love.graphics.setColor(1, 1, 1)
   love.graphics.draw(background, 0, 0)
+  -- love.graphics.draw(whitePiece, 1000, 1000)
+  -- love.graphics.draw(blackPiece, 1200, 1244)
+  drawPieces()
   love.graphics.setColor(0, 0, 1)
   love.graphics.print(love.mouse.getX()..", "..love.mouse.getY(), 1000, 1000)
   love.graphics.print(mouseCoordsX..", "..mouseCoordsY, 1000, 1100)
+  draw = false
+  -- end
 end
